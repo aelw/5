7d4f097e7cb491cfbde7afd97ad52ae9bbd2a4c8
aelw <89507166+aelw@users.noreply.github.com>
2021-09-06 09:45:30 +0900

    Create lisa.js

---
+++ dist/lisa.js
@@ -0,0 +1,91 @@
+var $messages = $('.messages-content'),
+    d, h, m,
+    i = 0;
+
+$(window).load(function() {
+  $messages.mCustomScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 100);
+});
+
+
+function updateScrollbar() {
+  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+    scrollInertia: 10,
+    timeout: 0
+  });
+}
+
+function setDate(){
+  d = new Date()
+  if (m != d.getMinutes()) {
+    m = d.getMinutes();
+    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    $('<div class="checkmark-sent-delivered">&check;</div>').appendTo($('.message:last'));
+    $('<div class="checkmark-read">&check;</div>').appendTo($('.message:last'));
+  }
+}
+
+function insertMessage() {
+  msg = $('.message-input').val();
+  if ($.trim(msg) == '') {
+    return false;
+  }
+  $('<div class="message message-personal">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
+  setDate();
+  $('.message-input').val(null);
+  updateScrollbar();
+  setTimeout(function() {
+    fakeMessage();
+  }, 1000 + (Math.random() * 20) * 100);
+}
+
+$('.message-submit').click(function() {
+  insertMessage();
+});
+
+$(window).on('keydown', function(e) {
+  if (e.which == 13) {
+    insertMessage();
+    return false;
+  }
+})
+
+var Fake = [
+  'Hi! It\'s great to see you!',
+  'How are you?',
+  'I\'m glad you\'re satisfied',
+  'What do you do?',
+  'That\'s awesome',
+  'I think you\'re a nice person',
+  'Why do you think that?',
+  'Can you explain?',
+  'Anyway I\'ve gotta go now',
+  'It was a pleasure chat with you',
+  'Call me when you think of me',
+  'See you',
+  ':)'
+]
+
+function fakeMessage() {
+  if ($('.message-input').val() != '') {
+    return false;
+  }
+  $('<div class="message loading new"><figure class="avatar"><img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMDRfMjg3/MDAxNDg4NjM3OTg5ODY4.ihaxi_0gVdJGDpDvGKXKtbUfdi0ngj6W0VUHjOF6b2Qg.dwQd7lD03Cky94ll3lXRjU65b8oPxqdxVrYJeXTxQ5cg.GIF.grace4088/25082B3E5888497C1BC1AF.gif?type=w800" /></figure><span></span></div>').appendTo($('.mCSB_container'));
+  updateScrollbar();
+
+  setTimeout(function() {
+    $('.message.loading').remove();
+    $('<div class="message new"><figure class="avatar"><img src="https://mblogthumb-phinf.pstatic.net/MjAxNzAzMDRfMjg3/MDAxNDg4NjM3OTg5ODY4.ihaxi_0gVdJGDpDvGKXKtbUfdi0ngj6W0VUHjOF6b2Qg.dwQd7lD03Cky94ll3lXRjU65b8oPxqdxVrYJeXTxQ5cg.GIF.grace4088/25082B3E5888497C1BC1AF.gif?type=w800" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
+    setDate();
+    updateScrollbar();
+    i++;
+  }, 1000 + (Math.random() * 20) * 100);
+
+}
+
+$('.button').click(function(){
+  $('.menu .items span').toggleClass('active');
+   $('.menu .button').toggleClass('active');
+});
